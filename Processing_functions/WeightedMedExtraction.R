eead4f63ec79b3bcb2148a4928a9df476a84d55c
aldotapia <aatapia@userena.cl>
2022-05-28 22:58:04 -0400

    Small fixes

--- Processing_functions/WeightedMedExtraction.R
+++ Processing_functions/WeightedMedExtraction.R
@@ -4,7 +4,7 @@
 # Copyright (c) 2022 Aldo Tapia.
 #
 # ATTENTION!
-# This script was made for extracting the weighted mean of pixels
+# This script was made for extracting the weighted median of pixels
 # using a vector file with THE SAME CRS THAN THE RASTER FILE. The
 # vector reprojection is skipped here in order to keep the memory
 # usage as low as possible. Be aware than the field used for appending
--- Processing_functions/WeightedStdExtraction.R
+++ Processing_functions/WeightedStdExtraction.R
@@ -4,8 +4,8 @@
 # Copyright (c) 2022 Aldo Tapia.
 #
 # ATTENTION!
-# This script was made for extracting the weighted mean of pixels
-# using a vector file with THE SAME CRS THAN THE RASTER FILE. The
+# This script was made for extracting the weighted standard deviation of 
+# pixels using a vector file with THE SAME CRS THAN THE RASTER FILE. The
 # vector reprojection is skipped here in order to keep the memory
 # usage as low as possible. Be aware than the field used for appending
 # ID column to value column here is `gauge_id`
