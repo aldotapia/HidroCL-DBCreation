bb53431b07ff13659b065a9b242b570c3c490509
aldotapia <aatapia@userena.cl>
2022-06-24 09:16:57 -0400

    Bug fix in persiann download

--- DownloadFunctions/persiann_download.py
+++ DownloadFunctions/persiann_download.py
@@ -41,9 +41,9 @@ files_list = [value.split(' ')[-1] for value in dir_list if 'bin' in value]
 files_list = [value for value in files_list if value.split('.gz')[0] not in os.listdir(product_path)]
 
 for file in files_list:
-    file_link = os.path.join('ftp://',ftp_server,ftp_path,files_list[1])
+    file_link = os.path.join('ftp://',ftp_server,ftp_path,file)
     file_path = os.path.join(product_path,file)
-    print(f'Downloading {file}')
+    print(f'\nDownloading {file}')
     wget.download(file_link, file_path)
     with gzip.open(file_path, 'r') as f_in, open(file_path.split('.gz')[0], 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
